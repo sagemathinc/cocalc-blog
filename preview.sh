aa2bf030bc08657c040a30b051647c751722b771
SageMath, Inc <office@sagemath.com>
2019-06-10 12:00:57 +0000

    minor conf

--- preview.sh
+++ preview.sh
@@ -10,7 +10,7 @@ cp _config _config_preview.yml
 sed -i "s|baseurl: \"\"|baseurl: \"$BASE\"|" _config_preview.yml
 
 echo; echo; echo; echo
-echo ">>>>>>>>>>>> open browser in https://cloud.sagemath.com$BASE <<<<<<<<<<<<<"
+echo ">>>>>>>>>>>> open browser in https://cocalc.com$BASE <<<<<<<<<<<<<"
 echo "             edits will compile when source files change, and 'future' posts will be shown, too!"
 echo; echo; echo; echo
 
